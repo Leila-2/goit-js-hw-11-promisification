147f047ff3af9e9a3be791432cbe173c36174e39
Leila-2 <liliya.yavorskaya17@gmail.com>
2021-09-27 17:26:23 +0300

    исправляет ошибку

--- task-3.js
+++ task-3.js
@@ -2,7 +2,7 @@ const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
+const makeTransaction = transaction => {
 
     let promise
     return promise = new Promise((resolve, reject) => {
@@ -12,7 +12,7 @@ const makeTransaction = (transaction) => {
             const canProcess = Math.random() > 0.3;
 
             if (canProcess) {
-                resolve(transaction.id, delay);
+                resolve({ id: transaction.id, time: delay });
             } else {
                 reject(transaction.id);
             }
@@ -22,7 +22,7 @@ const makeTransaction = (transaction) => {
 
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
